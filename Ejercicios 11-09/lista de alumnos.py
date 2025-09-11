c934ec94fb4ce1df9b5ec75f88f7b0044ad48cac
Ruka77 <luca.argumedo@gmail.com>
2025-09-11 09:08:36 -0300

    Ejercicios del día numero 11/09

---
+++ Ejercicios 11-09/lista de alumnos.py
@@ -0,0 +1,64 @@
+import random
+
+alumnos = [
+    "Valentina De Los ángeles Albizú",
+    "Pablo Andres Allende",
+    "Luca Valentín Argumedo",
+    "Pablo Avalos",
+    "Lucas Avila",
+    "Santino Barone",
+    "Sofia Blangetti",
+    "Nicolás Bohm",
+    "Renzo Valentino Borello Canizo",
+    "Juan Manuel Carrillo Taglio",
+    "Facundo Gustavo Chacon Catalan",
+    "Agustin Emiliano Contardi",
+    "Jeronimo Coronel Alvarez",
+    "Gabriel Emiliano Denis",
+    "Facundo Gustavo Deseff",
+    "Juan Martin García",
+    "Enzo Giaquinta",
+    "Sabrina Gimenez",
+    "Joaquin Godoy",
+    "Lucas Facundo Godoy",
+    "Santino Alejo Godoy Galdeano",
+    "Valentina Antonela Gordillo Moreno",
+    "Lautaro Agustin Guardatti Esquivel",
+    "Tiago Nahuel Guillot Duran",
+    "Mateo Lautaro Liendo",
+    "Juan Ignacio Martinez Quiroga",
+    "Maximo Exequiel Monardez",
+    "Tomas Agustin Mora Gonzalez",
+    "Pablo Isaias Morinigo Lima",
+    "Ares Martín Ocaña Martinez",
+    "Thiago Santino Oviedo Saldaño",
+    "Amanda Lucrecia Pagano",
+    "Máximo Juan Cruz Quiroga",
+    "Facundo Agustín Ramírez García",
+    "Franco Agustin Rios Alzamora",
+    "Leonel Enrique Rojas",
+    "Matias Nicolas Ruiz Vargas",
+    "Ramiro Ezequiel Salcedo",
+    "Ismael Saleme Padolsky",
+    "Ignacio Exequiel Sanchez",
+    "Fabrizio Jonathan Simon Santos",
+    "Cristian Gabriel Soto",
+    "Giovanna Mercedes Suarez",
+    "Ismael Mauricio Suarez",
+    "Fernando Agustín Torrez",
+    "Luca Nicolas Valdez",
+    "Tiziano Ignacio Valentini",
+    "Matias Nicolas Vargas",
+    "Juan Ignacio Videla Continella",
+    "Pablo Exequiel Avalos"
+]
+matriz = []
+lista_desordenada = random.sample(range(len(alumnos)), len(alumnos))
+
+for i in range(len(alumnos)//4):
+    matriz.append([])
+    for j in range(4):
+        matriz[i].append(alumnos[lista_desordenada.pop()])
+
+for i in matriz:
+    print(i)
